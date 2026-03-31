a43c157742711bc64434745d47b2b85bab8a2dfa
herdes <soaressantos02000@gmail.com>
2026-03-31 10:08:28 -0300

    menu hamburger

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,29 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
-import sun from '../../assets/images/sun.svg'
-import menu from '../../assets/images/menu.svg'
+import sun from "../../assets/images/sun.svg";
+import menu from "../../assets/images/menu.svg";
+import { useState } from "react";
 
 function Header() {
-    return(
-        <header className={styles.header}>
-            <div className={styles.modeLight}>
-                <img src={sun} alt="botão de mudar modo dark" />
-            </div>
-            <div className={styles.menu}>
-                <img src={menu} alt="botão de abrir menu" />
-            </div>
-        </header>
-    ) 
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.modeLight}>
+        <img src={sun} alt="botão de mudar modo dark" />
+      </div>
+      <nav className={`${styles.nav} ${open ? styles.open : ""}`}>
+        <a href="/">Home</a>
+        <a href="/">Sobre</a>
+        <a href="/">Habilidades</a>
+        <a href="/">Projetos</a>
+        <a href="/">Serviços</a>
+      </nav>
+      <div className={`${styles.menu} `} onClick={() => setOpen(!open)}>
+        <img src={menu} alt="botão de abrir menu" />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
