9595e3285e16bca0448edc8722a71f183d90e4bf
herdes <soaressantos02000@gmail.com>
2026-04-16 16:12:09 -0300

    Correção

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 import instagram from "../../assets/images/instagram.svg";
 import github from "../../assets/images/github_circle.svg";
-import linkedin from "../../assets/images/linkedIn_circle.svg";
+import linkedin from "../../assets/images/linkedIn.svg";
 
 function Footer() {
   return (
